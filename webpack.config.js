8d7854b6db545689823a983b71db8e1f52c43021
Julio Vasconez <jvasconez28@gmail.com>
2017-06-26 20:46:54 -0500

    Loaders & Plugins

--- webpack.config.js
+++ webpack.config.js
@@ -8,15 +8,65 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.js?$/,
+        test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015'],
-          plugins: ['transform-decorators-legacy'],
+          plugins: [ "transform-class-properties","transform-decorators-legacy"],
         }
       },
-      { test: /\.css$/, loader: "style-loader!css-loader" },
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+          plugins: [ "transform-class-properties","transform-decorators-legacy","transform-object-rest-spread"],
+        }
+      },
+      {
+        exclude: [
+          /\.html$/,
+          /\.(js|jsx)$/,
+          /\.css$/,
+          /\.json$/,
+          /\.bmp$/,
+          /\.gif$/,
+          /\.jpe?g$/,
+          /\.png$/,
+        ],
+        loader: require.resolve('file-loader'),
+        options: {
+          name: 'static/media/[name].[hash:8].[ext]',
+        },
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        loader: require.resolve('url-loader'),
+        options: {
+          limit: 10000,
+          name: 'static/media/[name].[hash:8].[ext]',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          require.resolve('style-loader'),
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader:'postcss-loader',
+            options: {
+              ident: 'postcss',
+            },
+          },
+        ],
+      },
     ]
   }
 }
